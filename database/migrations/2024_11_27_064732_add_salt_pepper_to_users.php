4171474a0790a84e39b391ebe13b6206b6678d56
KentVince <gonzaleskentvincent@gmail.com>
2024-11-27 14:53:08 +0800

    uploading_project

---
+++ database/migrations/2024_11_27_064732_add_salt_pepper_to_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->string('salt', 64)->after('password'); // Add salt column
+            $table->string('pepper', 64)->nullable()->after('salt'); // Add pepper column (optional)
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('salt');
+            $table->dropColumn('pepper');
+        });
+    }
+};
